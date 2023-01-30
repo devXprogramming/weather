80d94d8c242c67eef29740e41b129e5e2234e3c3
DevXprogramming <devxprogramming@gmail.com>
2023-01-30 16:05:34 +0000

    First Commit

--- weather/views.py
+++ weather/views.py
@@ -7,27 +7,34 @@ import urllib
 
 def home(request):
     if request.method=="POST":
-        userInput = request.POST.get('userInput')
 
-        apiKey = "593114135eaf0b3ba55ed528bdda7e46"
+        try:
+            userInput = request.POST.get('userInput')
 
-        url = urllib.request.urlopen(f'https://api.openweathermap.org/data/2.5/weather?q={userInput}&appid={apiKey}')
-        
-        res = json.load(url)
-        print(res)
+            apiKey = "593114135eaf0b3ba55ed528bdda7e46"
 
-        data = {
-            'city': userInput,
-            'temperature': res['main']['temp'],
-            'pressure': res['main']['pressure'],
-            'humidity': res['main']['humidity'],
-            'sealevel': res['main']['sea_level'],
-            'timezone': res['timezone'],
-            'country': res['sys']['country'],
-            'description': res['weather'][0]['description'],
-            'mode': res['weather'][0]['main'],
-            'icon': res['weather'][0]['icon'],
-        }
+            url = urllib.request.urlopen(f'https://api.openweathermap.org/data/2.5/weather?q={userInput}&appid={apiKey}')
+            
+            res = json.load(url)
+            print(res)
+
+            data = {
+                'city': userInput,
+                'temperature': res['main']['temp'],
+                'pressure': res['main']['pressure'],
+                'humidity': res['main']['humidity'],
+                'sealevel': res['main']['sea_level'],
+                'timezone': res['timezone'],
+                'country': res['sys']['country'],
+                'description': res['weather'][0]['description'],
+                'mode': res['weather'][0]['main'],
+                'icon': res['weather'][0]['icon'],
+            }
+        except:
+            show = {
+                'city': userInput,
+            }
+            return render(request, 'error.html', {'show':show})
     else:
         data = {
             'city':None,
